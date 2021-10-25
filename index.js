aa3c1077dc97e211c11a74155e0dbd0716a4dd2d
Caden J. Arthur <caden.j.arthur@gmail.com>
2021-10-25 16:34:47 -0400

    Added sql server connection and comments.

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
+/*
+* -------------------------------------------------------
+*
+*                       IMPORTS
+*
+* -------------------------------------------------------
+*/
 const express = require('express');
 const fs = require('fs');
 const mysql = require('mysql');
-// Grab database credentials
-const json = fs.readFileSync('credentials.json', 'utf8');
-const credentials = JSON.parse(json);
-// Open a connection to the database
 
+/*
+* -------------------------------------------------------
+*
+*                          SQL
+*
+* -------------------------------------------------------
+*/
 
+// Variable to hold SQL connection
+let connection = null;
+// Function to create the SQL connection
+function createSQLConnection() {
+    const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+    connection = mysql.createConnection(credentials);
+    connection.connect(error => {
+        if (error) {
+            console.error(error);
+            process.exit(-1);
+        }
+        console.log("An SQL connection was established to: " + credentials.database + " located at: " + credentials.host + " by: " + credentials.user);
+    });
+}
+// Function to query the SQL connection
+async function querySQLConnection(query) {
+    await connection.query(query, (error, rows) => {
+        if (error) {
+            return -1;
+        } else {
+            return rows;
+        }
+    });  
+}
 
+/*
+* -------------------------------------------------------
+*
+*                       SERVICE
+*
+* -------------------------------------------------------
+*/
 
-
-
-// Set up the service
+//  Set up the service
 const service = express();
 service.use(express.json());
 // Get all restaurants with the name
@@ -22,9 +61,21 @@ service.get('/:restaurantName', (request, response) => {
 // Get restaurant info based on its location
 
 
+// TODO
 
 
-//Specify the port and start the service
+
+/*
+* -------------------------------------------------------
+*
+*                      INITIALIZE
+*
+* -------------------------------------------------------
+*/
+
+// Set up the SQL connection
+createSQLConnection();
+// Start the web service
 const port = 8443;
 service.listen(port, () => {
     console.log("Drivethru webservice is live");
